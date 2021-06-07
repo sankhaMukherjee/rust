f45d7e6b9331322d4c782c19e13412db13b3cdf4
sankha <sankha.mukherjee@gmail.com>
2021-06-07 23:17:54 +0800

    minor changes to print.rs

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,14 @@
 pub fn run() {
     println!("Hello from the print.rs file ...");
+
+    println!("{} is form {} and {} likes to {}", "part1", "part2", 3, 12.4);
+
+    println!("{0} is form {1} and {1} likes to {0}", "part2", 12.4);
+
+    println!(
+        "{name} is form {city}",
+        name = "Sankha", city = "Singapore"
+    );
+
+    println!("{:?}", (12, true, "holla"));
 }
